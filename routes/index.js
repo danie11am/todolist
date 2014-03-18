dbff189405d44389b6ecdab51f9ed868b5e57739
Daniel Lam <im@daniellam.name>
2014-03-18 23:40:27 +1100

    Passed sample todo data in json format in index.js rendering call.
    This is Step 1 in the Part 2 tutorial in http://thecodebarbarian.wordpress.com/2013/07/29/introduction-to-the-mean-stack-part-two-building-and-testing-a-to-do-list/

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-	
+exports.index = function(req, res) {
+
 	// This renders a HTML page using the Jade language, in index.js.
-	res.render('index', { title: 'Express' });
+	res.render('index', {
+		title : 'Express',
+		todos : [{
+			description : "Buy eggs",
+			due : new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
+			done : false
+		}, {
+			description : "Write next blog post",
+			due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+			done : false
+		}, {
+			description : "Build todo list app",
+			due : new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+			done : true
+		}]
+	});
+
 };
