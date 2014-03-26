8d17968ebc1beee99134c5ca83b8fc58993d5500
Daniel Lam <im@daniellam.name>
2014-03-26 12:37:20 +1100

    Added comments only.

--- public/javascripts/controllers/TodoListController.js
+++ public/javascripts/controllers/TodoListController.js
@@ -1,3 +1,20 @@
+/*
+ * Controller class used by index.jade to serve frontend needs. 
+ * 
+ * This is included in the header of index.jade and also specified as an AngularJS controller using the 
+ * "ng-controller" attribute.
+ * 
+ * This is an AngularJS feature/convention.
+ * 
+ * The arguments to the constructor are set automatically by AnguarlJS. This feature is known as 
+ * "dependency injection". Here is a succint explanation from stackoverflow 
+ * (http://stackoverflow.com/questions/130794/what-is-dependency-injection ) on what that means:
+ * 
+ * "Basically, instead of having your objects creating a dependency or asking a factory object to make one for them, 
+ * you pass the needed dependencies in to the constructor or via property setters, and you make it somebody else's 
+ * problem"
+ *  
+ */
 function TodoListController($scope, $http, $timeout) {
   $scope.todos = [];
   $scope.newTodo = {
